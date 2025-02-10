3e4afcbd74fa8dd022cc190ac534024da958ee36
boxuk-wp-robot <developers@boxuk.com>
2025-02-10 14:25:18 +0000

    Merge pull request #340 from boxuk/php-test-exteions

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n', 'wp-url'), 'version' => '772887483cc769045d14');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n', 'wp-url'), 'version' => 'cd6fa4b5553f06841a2e');
